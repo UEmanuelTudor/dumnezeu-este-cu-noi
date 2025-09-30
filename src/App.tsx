57827b67adeb7972d5b4c5f44e29b0931ef952c9
UEmanuelT <uifalean_e@yahoo.com>
2025-09-30 09:13:39 +0300

    initial commit
    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-6">
+      <div className="p-4 bg-blue-500 text-white rounded-lg shadow-lg">
+        Tailwind merge! 🎉
+      </div>
+    </div>
+  )
+}
